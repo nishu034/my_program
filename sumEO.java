1f97eb268f027640404f743ab87178dbf7311cad
nishu034 <gone.sreenishruth04@gmail.com>
2026-07-27 23:53:10 +0530

    Add sumEO.java to check even or odd sum

---
+++ sumEO.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        if((n+m) % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+        
+    }
+}
